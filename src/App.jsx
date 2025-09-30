32d9da0c9f8b31861616e33da40333ad75ba36a0
aynalhaque <aynalhaque7411@gmail.com>
2025-09-30 00:03:07 -0700

    aynal.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Navbar from "./components/Navbar";
 import './App.css'
-
+import Aynal from './components/Aynal';
 function App() {
  
 
@@ -12,6 +12,7 @@ function App() {
       <h1>Welcome To My React website</h1>
       <p>This is a simple React application with a Navbar component.</p>
         </div>
+        <Aynal />
     </>
   )
 }
---
+++ src/components/Aynal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Aynal.css'
+
+export default function Aynal() {
+  return (
+    <div className='aynal'>
+      <h1>Hi User</h1>
+      <h1>I am Aynal haque Milon</h1>
+      <p>I am MERN stack developer </p>
+    </div>
+  )
+}
